d576f02b2060e73668ef78fbac609ba8ef625ec7
Agnieszka <aagnieszka.zurek@gmail.com>
2023-02-15 11:32:22 +0100

    css styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
 export const Home = () => {
   return (
-    <img
-      id="background-image"
-      src="https://www.realsimple.com/thmb/Tt5tGYHE2EhlpAMWZyAzkgJr-vI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/fresh-pumpkin-pie-recipe-2-2000-38a8be0c80094dc0ac691b90128ddb50.jpg"
-    />
+    <>
+      <div id="background-image">
+        <div id="order-button-container">
+          <button id="order-button" class="buttons">
+            Złóż zamówienie{" "}
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
