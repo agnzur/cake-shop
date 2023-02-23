aa8dd1ba6882e4f13f796e87102585931fdc6239
Agnieszka <aagnieszka.zurek@gmail.com>
2023-02-23 14:29:38 +0100

    regex for special characters added

--- src/components/summary/SubmitButton.js
+++ src/components/summary/SubmitButton.js
@@ -2,11 +2,14 @@ import { Link } from "react-router-dom";
 
 export const SubmitButton = ({ name, lastName, email, orderDate }) => {
   const numRegex = new RegExp("[0-9]");
+  const charactersRegex = new RegExp("^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+$");
   const isButtonDisabled =
     name.length < 3 ||
     numRegex.test(name) ||
-    numRegex.test(lastName) ||
+    !charactersRegex.test(name) ||
     lastName.length < 3 ||
+    numRegex.test(lastName) ||
+    !charactersRegex.test(lastName) ||
     email.length < 3 ||
     !email.includes("@") ||
     orderDate < new Date();
