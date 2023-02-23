db9a2acdb0d3119131f4bfae50ddc20651c211fe
Agnieszka <aagnieszka.zurek@gmail.com>
2023-02-23 14:41:32 +0100

    regex improved

--- src/components/summary/SubmitButton.js
+++ src/components/summary/SubmitButton.js
@@ -1,26 +1,27 @@
 import { Link } from "react-router-dom";
 
 export const SubmitButton = ({ name, lastName, email, orderDate }) => {
-  const numRegex = new RegExp("[0-9]");
-  const charactersRegex = new RegExp("^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+$");
-  const isButtonDisabled =
-    name.length < 3 ||
-    numRegex.test(name) ||
-    !charactersRegex.test(name) ||
-    lastName.length < 3 ||
-    numRegex.test(lastName) ||
-    !charactersRegex.test(lastName) ||
-    email.length < 3 ||
-    !email.includes("@") ||
-    orderDate < new Date();
+  const isButtonDisabled = () => {
+    const numRegex = new RegExp("[0-9]");
+    const charactersRegex = new RegExp("^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+$");
+    if (name.length < 3) return true;
+    if (numRegex.test(name)) return true;
+    if (!charactersRegex.test(name)) return true;
+    if (lastName.length < 3) return true;
+    if (numRegex.test(lastName)) return true;
+    if (!charactersRegex.test(lastName)) return true;
+    if (email.length < 3) return true;
+    if (!email.includes("@")) return true;
+    if (orderDate < new Date()) return true;
+  };
 
   return (
     <div className="submit-button-container">
       <Link to="/confirmation">
         <button
           id="submit-button"
-          className={isButtonDisabled ? "submit-disabled" : "submit-enabled"}
-          disabled={isButtonDisabled}
+          className={isButtonDisabled() ? "submit-disabled" : "submit-enabled"}
+          disabled={isButtonDisabled()}
         >
           Potwierdź
         </button>
